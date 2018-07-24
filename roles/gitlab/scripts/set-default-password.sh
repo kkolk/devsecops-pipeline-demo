5e6fd7e0f859ecfcf4b6a694447f0f883cb1c4e2
Kevin Kolk <kkolk@frostbyte.us>
2018-07-24 15:26:03 -0400

    Solved initial pasword setup issues.

--- roles/gitlab/scripts/set-default-password.sh
+++ roles/gitlab/scripts/set-default-password.sh
@@ -1,5 +1,8 @@
 PASS=$1
-gitlab-rails runner -e production "  \
-  user = User.where(id: 1).first               \
-  user.password = user.password_confirmation = '$PASS'; \
-  user.save!"
+# gitlab-rails runner -e production " \
+#   user = User.where(id: 1).first;               \
+#   user.password = user.password_confirmation = '$PASS'; \
+#   user.save!"
+
+gitlab-rails dbconsole " \
+    select authentication_token from users where id=1!"
